1b7f8ee7e07cf1ddbed76c505fd5147f67004aab
Hajira Mohamed Ali <hajmoham@lab3r2s8.42abudhabi.ae>
2025-06-04 10:02:46 +0400

    DONE

--- philo_clean.c
+++ philo_clean.c
@@ -6,12 +6,13 @@
 /*   By: hajmoham <hajmoham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 16:46:04 by hajmoham          #+#    #+#             */
-/*   Updated: 2025/06/03 16:46:05 by hajmoham         ###   ########.fr       */
+/*   Updated: 2025/06/04 10:02:07 by hajmoham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+// Cleans up resources used by the simulation
 void	clean(t_data *data)
 {
 	unsigned long long	i;
